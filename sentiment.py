50858da5a83ceacc6d839af55c49b609043c6ccd
Daniel Soares <ds507556@gmail.com>
2024-05-09 16:50:14 -0300

    Update commit

--- sentiment.py
+++ sentiment.py
@@ -3,20 +3,20 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import streamlit as st
 import nltk
 
-st.write('# Qual a sua opinião?')
-user_input = st.text_input('Escreva aqui: ')
-text_t = GoogleTranslator(source='pt', target='en').translate(user_input)
+st.write('### How do you feel?')
+user_input = st.text_input('Type here (use one or two words): ')
+#text_t = GoogleTranslator(source='pt', target='en').translate(user_input)
 
 nltk.download('vader_lexicon')
 
 analyzer = SentimentIntensityAnalyzer()
-score = analyzer.polarity_scores(text_t)
+score = analyzer.polarity_scores(user_input)
     
 if score['neu'] + score['neg'] > score['pos'] and score['neg'] != 0:
-    st.write("### Sentimos muito :(")
+    st.write("### Sorry :(")
 
 elif (score['pos'] > score['neu'] or score['pos'] > score['neg']) and score['neg'] == 0:
-    st.write('### Nós agradecemos!')    
+    st.write('### Awesome! :)')    
     
 
 
